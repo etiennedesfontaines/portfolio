cb739afd7cd670fded97cfc10db1e5c72299767c
Etienne Desfontaines <erdesfontaines@gmail.com>
2023-08-02 22:16:40 +0100

    data submission handling

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 
 //data
-import { colours } from "../styles/styleVariables";
+// import { colours } from "../styles/styleVariables";
 import LayoutColumn from "./LayoutColumn";
 
 const ContactForm = ({ isMobile }) => {
